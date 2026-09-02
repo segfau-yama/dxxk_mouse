4e0553396cc2c2116f7e9ac2ccd9893445035af0
Ymym <47805973+segfau-yama@users.noreply.github.com>
2026-09-02 09:55:04 +0900

    Migrate main peripheral test to esp-hal 1.2 API

--- dick_mouse/tests/main.rs
+++ dick_mouse/tests/main.rs
@@ -8,12 +8,11 @@ esp_bootloader_esp_idf::esp_app_desc!();
 #[embedded_test::tests]
 mod tests {
     use esp_hal::{
-        i2s::master::{Channels, Config as I2sConfig, DataFormat, I2s},
-        interrupt::software::SoftwareInterruptControl,
-        otg_fs::Usb,
+        i2s::master::{Channels, DataFormat, I2s, TdmConfig as I2sConfig},
         pcnt::Pcnt,
         time::Rate,
         timer::timg::TimerGroup,
+        usb::otg::Usb,
     };
 
     const I2S_FRAME_BYTES: usize = 48 * core::mem::size_of::<i32>();
@@ -21,7 +20,6 @@ mod tests {
     #[test]
     fn mainで使うperipheralを初期化できる() {
         let peripherals = esp_hal::init(esp_hal::Config::default());
-        let sw_int = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
         let timg0 = TimerGroup::new(peripherals.TIMG0);
         let _pcnt = Pcnt::new(peripherals.PCNT);
         let (rx_descriptors, tx_descriptors) =
@@ -48,8 +46,8 @@ mod tests {
             .with_ws(peripherals.GPIO9)
             .with_dout(peripherals.GPIO10)
             .build(tx_descriptors);
-        let _usb = Usb::new(peripherals.USB0, peripherals.GPIO20, peripherals.GPIO19);
-        let _software_interrupt0 = sw_int.software_interrupt0;
+        let _usb = Usb::new_fs(peripherals.USB_FS, peripherals.GPIO20, peripherals.GPIO19);
+        let _software_interrupt0 = peripherals.FROM_CPU_INTR0;
         let _timer0 = timg0.timer0;
     }
 }
